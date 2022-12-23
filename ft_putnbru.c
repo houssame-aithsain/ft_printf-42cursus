69e41b3eeaab0b46b11f593b559b4f747e98f482
Houssame Ait hsain <hossame@inbox.ru>
2022-12-23 18:52:49 +0100

    6:56:50

---
+++ ft_putnbru.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbru.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 17:48:20 by hait-hsa          #+#    #+#             */
+/*   Updated: 2022/10/26 17:13:00 by hait-hsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putnbru(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	len = ft_count_numb(n);
+	if (n < 0)
+	{
+		ft_putchar(45);
+		ft_putnbru(n * -1);
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + 48);
+	if (n > 9)
+	{
+		ft_putnbru(n / 10);
+		ft_putnbru(n % 10);
+	}
+	return (len);
+}
